1513b45f853841c802c8f25a95b663cfcf0c11a3
Ismail Patel <ismail@credia.co.uk>
2020-09-09 15:55:48 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    theme: {
+        fontFamily: {
+        },
+        extend: {
+            colors: {
+
+            },
+            fontFamily: {
+
+            },
+            zIndex: {
+                '-1': '-1',
+            },
+        },
+    },
+    variants: {},
+    purge: {
+        content: [
+            './app/**/*.php',
+            './resources/**/*.html',
+            './resources/**/*.js',
+            './resources/**/*.jsx',
+            './resources/**/*.ts',
+            './resources/**/*.tsx',
+            './resources/**/*.php',
+            './resources/**/*.vue',
+            './resources/**/*.twig',
+        ],
+        options: {
+            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+        },
+    },
+    plugins: [
+        require('@tailwindcss/ui'),
+        require('@tailwindcss/typography'),
+    ],
+};
