7ad4fa8180e5ff383d5137b069ed849105752494
a-killer <mail@theobaldgranger.com>
2020-10-14 22:48:34 +0100

    Borkett - code - start -

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,8 @@
 @section('content')
 <div class="preloader">
   <div>
-    <h1 class="mb-12 tracking-widest text-center text-4xl">HB1</h1>
+    <h1 class="mb-12 tracking-widest text-center text-4xl">JACK BORKETT</h1>
   </div>
   <div class="circles"></div>
-  <div>
-    <p class="text-sm leading-snug max-w-md text-center">HB1 is a software development and interaction studio specialising in agile development and progressive technologies</p>
-  </div>
 </div>
 @endsection
